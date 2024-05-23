cb1bedae888dc773436e9d359a74fbcd9eb95373
reis-raquel <raquel.a.reis@gmail.com>
2024-05-23 14:28:50 +0300

    search engine java script

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 
-
-let searchElement= document.querySelector("#search-input");
-let cityElement=document.querySelector("#current-city");
-
+let apiUrl:"https://api.shecodes.io/weather/v1/current?query={query}&key={key}"
