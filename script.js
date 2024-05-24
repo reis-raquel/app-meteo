3e56c7fe5d64d0f157f0dbc805861bf478d158ba
reis-raquel <raquel.a.reis@gmail.com>
2024-05-24 23:13:38 +0300

    apiKey and  apiUrl

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function refreshWeather (response){
+  let temperatureElement=document.querySelector("#temperature");
+let temperature=response.data.temperature.current;
+  let cityElement=document.querySelector("#city");
+
+  cityElement.innerHTML=response.data.city;
+  temperatureElement.innerHTML=Math.round(temperature);
+}
+
+
+function searchCity(city) {
+  let apiKey = "dt3bbe9160a34o69bead9ceff8624028";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+console.log(apiUrl);
+
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-form-input");
@@ -8,6 +25,7 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+ 
+searchCity("Helsinki");
+ 
 
-
-let apiUrl:"https://api.shecodes.io/weather/v1/current?query={query}&key={key}"
