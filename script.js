bbc9e979e492d7b954a003198ef2f41e923f11a0
reis-raquel <raquel.a.reis@gmail.com>
2024-05-26 15:58:45 +0300

    insert hour and date

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 function refreshWeather (response){
-  let temperatureElement=document.querySelector("#temperature");
+
+let temperatureElement=document.querySelector("#temperature");
 let temperature=response.data.temperature.current;
-  let cityElement=document.querySelector("#city");
+let cityElement=document.querySelector("#city");
 let descriptionElement=document.querySelector("#description");
 let humidityElement=document.querySelector("#humidity");
-  let windSpeedElement=document.querySelector("#wind-speed");
+let windSpeedElement=document.querySelector("#wind-speed");
 let timeElement=document.querySelector("#time");
 let date = new Date(response.data.time*1000);
 
-  console.log(response.data);
 
 
-timeElement.innerHTML= date.getHours();
+timeElement.innerHTML= formatDate(date);
 windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
 humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
 descriptionElement.innerHTML=response.data.condition.description;
-  cityElement.innerHTML=response.data.city;
-  temperatureElement.innerHTML=Math.round(temperature);
+cityElement.innerHTML=response.data.city;
+temperatureElement.innerHTML=Math.round(temperature);
 }
 
+
+function formatDate(date){
+
+let minutes=date.getMinutes();
+let hours=date.getHours(); 
+let days= [ 
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "thursday", 
+  "Friday",
+  "Saturday"];
+
+  let day=days[date.getDay()];
+
+ return `${day}, ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
   let apiKey = "dt3bbe9160a34o69bead9ceff8624028";
