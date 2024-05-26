b9d2360c03194d89e640cc417cf76ab73662db1f
reis-raquel <raquel.a.reis@gmail.com>
2024-05-26 16:58:08 +0300

    injecting the image url_icon

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ let humidityElement=document.querySelector("#humidity");
 let windSpeedElement=document.querySelector("#wind-speed");
 let timeElement=document.querySelector("#time");
 let date = new Date(response.data.time*1000);
+let iconElement= document.querySelector("#icon");
 
-
+iconElement.innerHTML=`<img src="${response.data.condition.icon_url}" class="weather-icon"/>`
 
 timeElement.innerHTML= formatDate(date);
 windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
@@ -35,11 +36,15 @@ let days= [
 
   let day=days[date.getDay()];
 
+  if (minutes <10){
+    minutes = `0${minutes}`;
+  }
+
  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
-  let apiKey = "dt3bbe9160a34o69bead9ceff8624028";
+let apiKey = "dt3bbe9160a34o69bead9ceff8624028";
 let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(refreshWeather);
 
