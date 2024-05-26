b4a867daaadd0209912c70af2ff54bc32a1db7cc
reis-raquel <raquel.a.reis@gmail.com>
2024-05-26 15:25:19 +0300

    description of weather, humidity and wind

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ function refreshWeather (response){
   let temperatureElement=document.querySelector("#temperature");
 let temperature=response.data.temperature.current;
   let cityElement=document.querySelector("#city");
+let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
+  let windSpeedElement=document.querySelector("#wind-speed");
+let timeElement=document.querySelector("#time");
+let date = new Date(response.data.time*1000);
 
+  console.log(response.data);
+
+
+timeElement.innerHTML= date.getHours();
+windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
+humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+descriptionElement.innerHTML=response.data.condition.description;
   cityElement.innerHTML=response.data.city;
   temperatureElement.innerHTML=Math.round(temperature);
 }
