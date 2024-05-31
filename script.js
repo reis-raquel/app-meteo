23628895c5855ea41d8e5df939056099e0ef0dac
reis-raquel <raquel.a.reis@gmail.com>
2024-05-31 20:46:33 +0300

    css improve

--- script.js
+++ script.js
@@ -58,6 +58,15 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp){
+  let date = new Date(timestamp*1000);
+  let days = ["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"];
+ 
+  return days[date.getDay()];
+
+}
+
+
 
 function getForecast (city){
   let apiKey = "dt3bbe9160a34o69bead9ceff8624028";
@@ -66,32 +75,33 @@ function getForecast (city){
 }
 
 function displayForecast(response){
- console.log(response.data);
-
-  let days = ["Sun","Mon","Tue", "Wed", "thu"];
-
+ 
 let forecastHtml = "";
 
+  response.data.daily.forEach(function (day, index) {
 
-
-  days.forEach(function (day) {
-
-
+if (index < 5 ) {
     forecastHtml =
       forecastHtml +
       `
 <div class="weather-forecast-day">
-          <div class=" weather-forecast-date"> ${day}</div>
-          <div class="weather-forecast.icon"> ⛅
+          <div class=" weather-forecast-date"> ${formatDay(day.time)}</div>
+
+  
+           <img src="${day.condition.icon_url}"class="weather-forecast-icon"/>
+          
+          <div class="weather-forecast-temperatures">
+            <div class=" weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
           </div>
-          <div class="weather-forecast-temperatures>
-          <div class=" weather-forecast-temperature">
-            <strong>15°</strong>
-          </div>
-          <div class="weather-forecast-temperture">9°</div>
+          
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}°</div>
         </div>
       </div>
     `;
+        }
   });
 
   let forecastElement = document.querySelector("#forecast");
